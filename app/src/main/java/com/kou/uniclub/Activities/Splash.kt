900e0775b1cb5bee0b2ccd0e399d1387e1ce6128
KOU <elbehi.koutheir@gmail.com>
2019-05-26 00:02:42 -0400

    RxJava power wowo I wonder if Kotlin couroutines is jealous

--- app/src/main/java/com/kou/uniclub/Activities/Splash.kt
+++ app/src/main/java/com/kou/uniclub/Activities/Splash.kt
@@ -1,32 +1,45 @@
 package com.kou.uniclub.Activities
 
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.Handler
 import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.WindowManager
-import com.kou.uniclub.Extensions.Connectivity
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
+import com.kou.uniclub.Extensions.ConnectivityReceiver
 import com.kou.uniclub.R
 import com.kou.uniclub.SharedUtils.PrefsManager
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class Splash : AppCompatActivity() {
 
+
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //No Status Bar
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        registerReceiver(
+            ConnectivityReceiver(),
+            IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
         )
+
         if (PrefsManager.getFirstTime(this@Splash)) {
-            PrefsManager.setFirstime(this, false)
-
             setContentView(R.layout.activity_splash)
-
+            PrefsManager.setFirstime(this, false)
 
             val r = Runnable {
                 startActivity(Intent(this@Splash, Intro::class.java))
@@ -35,32 +48,56 @@ class Splash : AppCompatActivity() {
             val h = Handler()
             h.postDelayed(r, 2000) // will be delayed for 2 seconds
 
-        }
+        } else {
 
-        else{
-            //TODO("getLogged_status -> Home else -> Sign in")
             setContentView(R.layout.activity_splash)
+            ReactiveNetwork
+                .observeInternetConnectivity()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { isConnectedToInternet ->
+                    if (isConnectedToInternet) {
+                        val rotation = AnimationUtils.loadAnimation(this@Splash, R.anim.loading_icon)
+                        rotation.repeatCount = Animation.INFINITE
+                        imLoading.animation = rotation
+                        val r = Runnable {
+                            startActivity(Intent(this@Splash, Home::class.java))
+                            finish()
+                        }
+                        val h = Handler()
+                        h.postDelayed(r, 2000)
+                    } else {
+                        imLoading.visibility = View.INVISIBLE
 
-            if (Connectivity.isConnected(this@Splash)) {
+                        val snacko = Snackbar.make(imLogo, "Verify your network status", Snackbar.LENGTH_INDEFINITE)
+                        snacko.config(this@Splash)
+                        snacko.setAction("REFRESH") {
+                            imLoading.visibility = View.VISIBLE
+                            val rotation = AnimationUtils.loadAnimation(this@Splash, R.anim.loading_icon)
+                            rotation.repeatCount = Animation.INFINITE
+                            imLoading.animation = rotation
 
-                //startActivity(Intent(this@Splash, Auth::class.java))
-            }
-            else {
-                Snackbar.make(swiperefresh, "No internet! Swipe to refresh", Snackbar.LENGTH_LONG).show()
-
-                swiperefresh.setOnRefreshListener {
-
-                    if (Connectivity.isConnected(this@Splash))
-                        //startActivity(Intent(this@Splash, Auth::class.java))
-                    else {
-                        Snackbar.make(swiperefresh, "No internet", Snackbar.LENGTH_SHORT).show()
-                        swiperefresh.isRefreshing = false
+                        }
+                        snacko.show()
                     }
-
                 }
 
-            }
 
         }
+    }
+
+    fun Snackbar.config(context: Context) {
+        val params = this.view.layoutParams as ViewGroup.MarginLayoutParams
+        params.setMargins(12, 24, 12, 12)
+        this.view.layoutParams = params
+        this.view.background = ContextCompat.getDrawable(context, R.drawable.bg_snackbar)
+
+        this.setActionTextColor(ContextCompat.getColor(context, R.color.orange))
+        val text = this.view.findViewById<TextView>(android.support.design.R.id.snackbar_text)
+        text.setTextColor(ContextCompat.getColor(context, R.color.white))
+        text.maxLines = 1
+        text.textSize = 12f
+
+        ViewCompat.setElevation(this.view, 6f)
     }
 }
